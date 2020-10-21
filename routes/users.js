26558692b418ebc75b5af546550dc374a2a6798f
devenock <enockomondi305@gmail.com>
2020-10-21 05:54:01 -0400

    Adding routes

--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,37 @@ const express = require('express');
 // router
 const router = express.Router();
 
+// declare users array
+const users = [{
+        firstName: "John",
+        lastName: "Doe",
+        age: 23
+    },
+    {
+        firstName: "Jane",
+        lastName: "Doe",
+        age: 25
+    }
+]
+
 // ROUTES
 
 // GET
 router.get('/', (req, res) => {
-    res.send('This is the get route');
+    res.send(users);
 });
+
+// POST
+
+router.post('/', (req, res) => {
+
+    // declare user
+    const user = req.body;
+    // adding user to the users array
+    users.push(user);
+
+    res.send(`User with the name ${user.firstName} has been added to the database`);
+});
+
 
 module.exports = router;
